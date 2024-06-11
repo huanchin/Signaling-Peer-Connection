1a877ea0ff3471577fed7f1809ba3797d72a7299
huan-chin chen <cherry032826@gmail.com>
2024-06-11 21:17:26 +0800

    finished step 15

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+const userName = "Huan-" + Math.floor(Math.random() * 100000);
+const password = "x";
+document.querySelector("#user-name").innerHTML = userName;
+
+// 10) CLIENT2 loads up the webpage with io.connect()
+const socket = io.connect("https://localhost:3000", {
+  auth: {
+    userName,
+    password,
+  },
+});
+
 const localVideoEl = document.querySelector("#local-video");
 const remoteVideoEl = document.querySelector("#remote-video");
 
 let localStream; //a var to hold the local video stream
 let remoteStream; //a var to hold the remote video stream
 let peerConnection; //the peerConnection that the two clients use to talk
+let didIOffer = false;
 
 let peerConfiguration = {
   iceServers: [
@@ -12,21 +25,6 @@ let peerConfiguration = {
     },
   ],
 };
-
-/** 
-const fetchUserMedia = async () => {
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      video: true,
-      // audio: true,
-    });
-    localVideoEl.srcObject = stream;
-    localStream = stream;
-  } catch (err) {
-    console.log(err);
-  }
-};
-*/
 
 const fetchUserMedia = () =>
   new Promise(async (resolve, reject) => {
@@ -44,41 +42,45 @@ const fetchUserMedia = () =>
     }
   });
 
-/*
-const createPeerConnection = async () => {
-  try {
-    //RTCPeerConnection is the thing that creates the connection
-    //we can pass a config object, and that config object can contain stun servers
-    //which will fetch us ICE candidates
-    peerConnection = await new RTCPeerConnection(peerConfiguration);
-    peerConnection.addEventListener("icecandidate", (e) => {
-      console.log("......ICE Candidate event......");
-      console.log(e);
-    });
-  } catch (err) {
-    console.log(err);
-  }
-};
-*/
-
-const createPeerConnection = () =>
+const createPeerConnection = (offerObj) =>
   new Promise(async (resolve, reject) => {
     try {
       // RTCPeerConnection is the thing that creates the connection
       // we can pass a config object, and that config object can contain stun servers
       // which will fetch us ICE candidates
-      // 2-1)
+      // 3) peerConnection needs STUN servers, we will need ICE candidates later
       peerConnection = await new RTCPeerConnection(peerConfiguration);
 
-      // 2-2) Adds each track from the stream to the peer connection
+      // 4) CLIENT1 add localstream tracks to peerConnection
+      // 14) CLIENT2 adds localstream tracks to peerconnection
       localStream.getTracks().forEach((track) => {
         peerConnection.addTrack(track, localStream);
       });
 
+      // 7) ICE candidates can now start coming in
+      // 18) when setLocalDescription, start collecting ICE candidates (ASYNC)
       peerConnection.addEventListener("icecandidate", (e) => {
         console.log("****** ICE Candidate event ******");
         console.log(e);
+        if (e.candidate) {
+          // 9) Once 7 happens, emit ICE c. up to signaling server
+          socket.emit("sendIceCandidateToSignalingServer", {
+            iceCandidate: e.candidate,
+            iceUserName: userName,
+            didIOffer,
+          });
+        }
       });
+
+      // 17) Because CLIENT2 has the offer, CLIENT2 can hand the offer to pc.setRemoteDescription
+      if (offerObj) {
+        // this won't be set when called from call();
+        // will be set when we call from answerOffer()
+        // console.log(peerConnection.signalingState) //should be stable because no setDesc has been run yet
+        await peerConnection.setRemoteDescription(offerObj.offer);
+        // console.log(peerConnection.signalingState) //should be have-remote-offer, because client2 has setRemoteDesc on the offer
+      }
+
       resolve();
     } catch (err) {
       console.log(err);
@@ -86,23 +88,54 @@ const createPeerConnection = () =>
     }
   });
 
+const answerOffer = async (offerObj) => {
+  // 12) CLIENT2 runs getUserMedia()
+  await fetchUserMedia();
+
+  // 13) CLIENT2 creates a peerConnection()
+  await createPeerConnection(offerObj);
+
+  // 15) CLIENT2 creates an answer (createAnswer());
+  const answer = await peerConnection.createAnswer({}); //just to make the docs happy
+
+  // 16) CLIENT2 hands answer to pc.setLocalDescription
+  await peerConnection.setLocalDescription(answer); //this is CLIENT2, and CLIENT2 uses the answer as the localDesc
+  console.log(offerObj);
+  console.log(answer);
+  // console.log(peerConnection.signalingState) //should be have-local-pranswer because CLIENT2 has set its local desc to it's answer (but it won't be)
+  //add the answer to the offerObj so the server knows which offer this is related to
+  offerObj.answer = answer;
+  //emit the answer to the signaling server, so it can emit to CLIENT1
+  //expect a response from the server with the already existing ICE candidates
+  const offerIceCandidates = await socket.emitWithAck("newAnswer", offerObj);
+  offerIceCandidates.forEach((c) => {
+    peerConnection.addIceCandidate(c);
+    console.log("======Added Ice Candidate======");
+  });
+  console.log(offerIceCandidates);
+};
+
 //when a client initiates a call
 const call = async (e) => {
   try {
-    // 1) get stream from GUM
+    // 1) Someone must getUserMedia() - CLIENT1/Init/Caller/Offerer
     await fetchUserMedia();
 
-    // 2) create peer connection and get ICE candidate
+    // 2) CLIENT1 creates RTCPeerConnection
     await createPeerConnection();
 
-    // 3) create offer (generate SDP)
+    // 5) CLIENT1 creates an offer
     const offerSessionDescription = await peerConnection.createOffer();
     console.log("****** offer/ SDP ******");
     console.log(offerSessionDescription);
+    didIOffer = true;
 
-    // 4) Changes the local description associated with the connection
+    // 6) CLIENT1 hands offer to pc.setLocalDescription
     // setLocalDescription will trigger icecandidate event
     peerConnection.setLocalDescription(offerSessionDescription);
+
+    // 8) CLIENT1 emits offer
+    socket.emit("newOffer", offerSessionDescription);
   } catch (err) {
     console.log(err);
   }
--- server.js
+++ server.js
@@ -1,10 +1,93 @@
+const https = require("https");
+const fs = require("fs");
 const path = require("path");
 const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
 
 app.use(express.static(path.join(__dirname)));
 
-app.listen(3000, () => {
+//we need a key and cert to run https
+//we generated them with mkcert
+// $ mkcert create-ca
+// $ mkcert create-cert
+const key = fs.readFileSync("cert.key");
+const cert = fs.readFileSync("cert.crt");
+
+// we change our express setup so we can use https
+// pass the key and cert to createServer on https
+const expressServer = https.createServer({ key, cert }, app);
+
+//create our socket.io server... it will listen to our express port
+const io = socketio(expressServer);
+
+const offers = [
+  // offererUserName
+  // offer
+  // offerIceCandidate
+  // answererUserName
+  // answer
+  // answerIceCandidate
+];
+
+const connectedSockets = [
+  //username, socketId
+];
+
+io.on("connection", (socket) => {
+  console.log("someone has connected");
+
+  const userName = socket.handshake.auth.userName;
+  const password = socket.handshake.auth.password;
+
+  if (password !== "x") {
+    socket.disconnect(true);
+    return;
+  }
+  connectedSockets.push({
+    socketId: socket.id,
+    userName,
+  });
+
+  // 11) socket.io emit out the RTCSessionDesc to the new client
+  // a new client has joined. If there are any offers available,
+  // emit them out
+  if (offers.length) {
+    socket.emit("availableOffers", offers);
+  }
+
+  // store offer of offerer
+  socket.on("newOffer", (newOffer) => {
+    offers.push({
+      offererUserName: userName,
+      offer: newOffer,
+      offerIceCandidates: [],
+      answererUserName: null,
+      answer: null,
+      answererIceCandidates: [],
+    });
+    // console.log(newOffer.sdp.slice(50))
+    //send out to all connected sockets EXCEPT the caller
+    console.log(offers.length);
+    socket.broadcast.emit("newOfferAwaiting", offers.slice(-1));
+  });
+
+  // store ice candidate of offerer
+  socket.on("sendIceCandidateToSignalingServer", (iceCandidateObj) => {
+    const { didIOffer, iceUserName, iceCandidate } = iceCandidateObj;
+    console.log(iceCandidate);
+    if (didIOffer) {
+      const offerInOffers = offers.find(
+        (o) => o.offererUserName === iceUserName
+      );
+      if (offerInOffers) {
+        offerInOffers.offerIceCandidates.push(iceCandidate);
+      }
+    }
+  });
+});
+
+expressServer.listen(3000, () => {
   console.log("App running on port 3000");
 });
